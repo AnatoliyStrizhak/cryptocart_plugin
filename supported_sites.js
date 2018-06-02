6001dbfa8b229fdae57c84cd4e5bf90dd510ae08
Anatoliy Strizhak <jrodger@yandex.ru>
2018-06-02 13:57:18 +0200

    Добавил firstvds в список поддерживаемых сайтов. Опалат по коду лицевого счета через qiwi

--- supported_sites.js
+++ supported_sites.js
@@ -1,5 +1,6 @@
 {"sites": [
       {"url": "aliexpress.com", "order": "номер заказа"},
-      {"url": "jino.ru", "order": "логин в системе"}
+      {"url": "jino.ru", "order": "логин в системе"},
+      {"url": "my.firstvds.ru", "order": "код лицевого счета"}
     ] 
 }
